b6ccd5c83e75f13f44781f5dfcc5bfd432247c00
Jeff Adams <jeffa@railtrac.com>
2019-09-10 14:10:01 -0500

    Add project files.

---
+++ GossBot.Classic.Limitless/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GossBot.Classic.Limitless
+{
+    public class GossBot
+    {
+        public static void Main(string[] args)
+            => new GossBot().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+
+        }
+    }
+}
