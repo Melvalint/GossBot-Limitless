5fc715b0c3f61a3f26985914f00e8508312fea67
Jeff Adams <jeffa@railtrac.com>
2019-09-10 16:53:16 -0500

    Created SQLite DB Context with EFCore.

---
+++ GossBot.Classic.Limitless.Data.Models/Entities/RaiderEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GossBot.Classic.Data.Models.Entities
+{
+    public class RaiderEntity
+    {
+        public int ID { get; set; }
+        public string RaiderName { get; set; }
+        public string Class { get; set; }
+        public string Spec { get; set; }
+    }
+}
